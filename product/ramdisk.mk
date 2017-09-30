51ee7ea32cb1dad058abea7f2f581f2b9d718a9f
Ethan Chen <intervigil@gmail.com>
2017-09-29 23:14:23 -0700

    A6020: Remove init.qcom.bt.sh
    * Just run the hci_qcomm_init binary instead
    
    Change-Id: If2b4b9fc738e729bd389f3f96e6843a4ca6eb558

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -2,7 +2,6 @@
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.usb.rc \
-    init.qcom.bt.sh \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,13 +15,6 @@ LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.ril.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
