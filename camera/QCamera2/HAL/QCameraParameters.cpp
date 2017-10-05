4fdf9207cefdb6d4ed6d403a1ac89439d0541d40
dev-harsh1998 <harshit.india@linaro.org>
2017-10-05 12:27:44 +0530

    A6020: QCamera2: Disable dis forever
    * According to logs its breaking video recording on apps like IMO & WeChat
    
    Signed-off-by: ghostrider-reborn <adithya.r02@outlook.com>

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -2986,6 +2986,7 @@ int32_t QCameraParameters::setMCEValue(const QCameraParameters& params)
  *==========================================================================*/
 int32_t QCameraParameters::setDISValue(const QCameraParameters& params)
 {
+/*
     const char *str = params.get(KEY_QC_DIS);
     const char *prev_str = get(KEY_QC_DIS);
     if (str != NULL) {
@@ -2994,6 +2995,7 @@ int32_t QCameraParameters::setDISValue(const QCameraParameters& params)
             return setDISValue(str);
         }
     }
+*/
     return NO_ERROR;
 }
 
@@ -5028,9 +5030,9 @@ int32_t QCameraParameters::initDefaultParameters()
     set(KEY_QC_SUPPORTED_MEM_COLOR_ENHANCE_MODES, enableDisableValues);
     setMCEValue(VALUE_ENABLE);
 
-    // Set DIS
+    /* Set DIS
     set(KEY_QC_SUPPORTED_DIS_MODES, enableDisableValues);
-    setDISValue(VALUE_DISABLE);
+    setDISValue(VALUE_DISABLE); */
 
     // Set Histogram
     set(KEY_QC_SUPPORTED_HISTOGRAM_MODES,
