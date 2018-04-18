0fe3f9b585a382aec61e857785d4d9bb2589a46a
harry8242 <mohamedharis97@gmail.com>
2018-04-18 14:44:22 +0530

    A6020: Switch to post boot script

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -2,12 +2,12 @@
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.usb.rc \
-    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom-ims.rc \
     init.qcom.mem.sh \
     init.qcom.ril.sh \
+    init.qcom.post_boot.sh \
     init.qti.ims.sh \
     ueventd.qcom.rc
 
