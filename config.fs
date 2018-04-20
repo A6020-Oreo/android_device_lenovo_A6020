74834626e944046dff8de2ecacbbc3438b91f49f
Ad!thya R <adithya.r02@outlook.com>
2018-04-20 12:53:25 +0530

    A6020: config.fs: Remove inexisting services
    * We don't ship pm-service and cnss-daemon.

--- config.fs
+++ config.fs
@@ -7,18 +7,6 @@ value:2951
 [AID_RFS_SHARED]
 value:2952
 
-[system/vendor/bin/cnss-daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
